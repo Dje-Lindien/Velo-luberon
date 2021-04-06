cf00259831a391cba0b30b0411dfe54723845429
Jeremy <72251531+Dje-Lindien@users.noreply.github.com>
2021-04-06 23:02:49 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,29 @@
+
+<head>
+
+<link rel="stylesheet" href="../../css/bootstrap.min.css">
+<link rel="stylesheet" href="css/header_footer.css">
+
+</head>
+
+<body>
+    
+    <!-- FOOTER  -->
+    <footer class="flex-wrap">
+        <div class="nav justify-content-around align-items-center h-30">
+            <ul class="nav flex-md-row">
+                <li><a href="/index.php">Accueil</a></li>
+                <li><a href="/catalogue.php">Catalogue</a></li>
+                <li><a href="/index.php">Mentions légales</a></li>
+                <li><a href="/index.php">CGV</a></li>
+            </ul>
+
+            <div class="">
+                <a class="navbar-brand" href="/index.php">
+                    <img src="../../libraries/img/logo_mondovelo.jpg" width="105" height="54" alt="" loading="lazy">
+                </a>
+            </div>
+        </div>
+    </footer>
+
+</body>
