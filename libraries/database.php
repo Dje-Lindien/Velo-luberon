0bf052d2e06e4ebe73cfc91f30fa04001dc2b8b9
Jeremy <72251531+Dje-Lindien@users.noreply.github.com>
2021-08-11 12:01:50 +0200

    Update database.php

--- libraries/database.php
+++ libraries/database.php
@@ -9,7 +9,7 @@
 
 function getPdo(): PDO
 {
-    $pdo = new PDO('mysql:host=localhost;dbname=veloluberon', 'root', '', [
+    $pdo = new PDO('mysql:host=localhost;dbname=*****', '*****', '*****', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
